2b2a0cc5fcf20d6b4c46e5770dec081b8c8074e5
Lafe Spietz <lafelabs@gmail.com>
2018-10-28 01:07:07 -0400

    fixed tree replicator directory paths which had old version without curves/ dir

--- curve/treereplicator.php
+++ curve/treereplicator.php
@@ -6,24 +6,24 @@
     $baseurl = explode("json",$url)[0];
 
     foreach($dna as $dirs){
-        mkdir($dirs);
-        mkdir($dirs."/svg");
-        mkdir($dirs."/javascript");
-        mkdir($dirs."/markdown");
-        mkdir($dirs."/json");
+        mkdir("curves/".$dirs);
+        mkdir("curves/".$dirs."/svg");
+        mkdir("curves/".$dirs."/javascript");
+        mkdir("curves/".$dirs."/markdown");
+        mkdir("curves/".$dirs."/json");
 
-        $data = file_get_contents($baseurl."/".$dirs."/javascript/topfunctions.txt");
-        $file = fopen($dirs."/javascript/topfunctions.txt","w");// create new file with this name
+        $data = file_get_contents($baseurl."/curves/".$dirs."/javascript/topfunctions.txt");
+        $file = fopen("curves/".$dirs."/javascript/topfunctions.txt","w");// create new file with this name
         fwrite($file,$data); //write data to file
         fclose($file);  //close file
 
-        $data = file_get_contents($baseurl."/".$dirs."/json/currentjson.txt");
-        $file = fopen($dirs."/json/currentjson.txt","w");// create new file with this name
+        $data = file_get_contents($baseurl."/curves/".$dirs."/json/currentjson.txt");
+        $file = fopen("curves/".$dirs."/json/currentjson.txt","w");// create new file with this name
         fwrite($file,$data); //write data to file
         fclose($file);  //close file
 
-        $data = file_get_contents($baseurl."/".$dirs."/markdown/equation.txt");
-        $file = fopen($dirs."/markdown/equation.txt","w");// create new file with this name
+        $data = file_get_contents($baseurl."/curves".$dirs."/markdown/equation.txt");
+        $file = fopen("curves/".$dirs."/markdown/equation.txt","w");// create new file with this name
         fwrite($file,$data); //write data to file
         fclose($file);  //close file
 
